4faccb5d697f5fea129736b4bcf4a4065557736a
Cris Marin <145811107+CrisMarin1211@users.noreply.github.com>
2025-11-20 19:36:20 -0500

    Refactor StorePage component and remove comments

--- ParcialWeb3/src/pages/store.tsx
+++ ParcialWeb3/src/pages/store.tsx
@@ -1,99 +1,26 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setProducts, removeProduct } from "../redux/productSlice";
-// import { addToCart } from "../redux/cartSlice";
-// import { useNavigate } from "react-router-dom";
-// import { RootState } from "../redux/store";
-
-// interface Product {
-//   id: number;
-//   title: string;
-//   price: number;
-//   category: string;
-//   description: string;
-//   image: string;
-// }
-
-// function StorePage() {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   const { products } = useSelector((state: RootState) => state.products);
-//   const { role } = useSelector((state: RootState) => state.userAuth);
-
-//   useEffect(() => {
-//     const fetchProducts = async () => {
-//       try {
-//         const response = await fetch("https://fakestoreapi.com/products?limit=20");
-//         const data: Product[] = await response.json();
-//         dispatch(setProducts(data));
-//       } catch (error) {
-//         console.error("Error fetching products:", error);
-//       }
-//     };
-
-//     fetchProducts();
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       {role === "manager" && (
-//         <button onClick={() => navigate("/admin/create")}>Crear Producto</button>
-//       )}
-//       <div className="product-grid">
-//         {products.map((product: Product) => (
-//           <div key={product.id} className="product-card">
-//             <h3>{product.title}</h3>
-//             <img src={product.image} alt={product.title} />
-//             <p>${product.price}</p>
-//             <p>{product.category}</p>
-//             {role === "manager" ? (
-//               <button onClick={() => dispatch(removeProduct(product.id))}>Eliminar</button>
-//             ) : (
-//               <button onClick={() => dispatch(addToCart(product))}>Agregar al Carrito</button>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default StorePage;
-
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { setProducts, removeProduct } from "../redux/productSlice";
 import { addToCart } from "../redux/cartSlice";
-import type { RootState, AppDispatch } from "../redux/store";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../redux/store";
 
 interface Product {
-  id: number;
-  title: string;
+id: number;
+title: string;
   price: number;
   category: string;
   description: string;
   image: string;
 }
 
-interface CartItem {
-  id: number;
-  name: string;
-  price: number;
-  quantity: number;
-  image: string;
-}
-
 function StorePage() {
-  const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const { products } = useSelector((state: RootState) => state.products);
-  const { role } = useSelector((state: RootState) => state.auth);
+  const { role } = useSelector((state: RootState) => state.userAuth);
 
- 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -108,23 +35,11 @@ function StorePage() {
     fetchProducts();
   }, [dispatch]);
 
-  const handleAddToCart = (product: Product) => {
-    const cartItem: CartItem = {
-      id: product.id,
-      name: product.title,
-      price: product.price,
-      quantity: 1,
-      image: product.image
-    };
-    dispatch(addToCart(cartItem));
-  };
-
   return (
     <div>
       {role === "manager" && (
         <button onClick={() => navigate("/admin/create")}>Crear Producto</button>
       )}
-
       <div className="product-grid">
         {products.map((product: Product) => (
           <div key={product.id} className="product-card">
@@ -132,11 +47,10 @@ function StorePage() {
             <img src={product.image} alt={product.title} />
             <p>${product.price}</p>
             <p>{product.category}</p>
-
             {role === "manager" ? (
               <button onClick={() => dispatch(removeProduct(product.id))}>Eliminar</button>
             ) : (
-              <button onClick={() => handleAddToCart(product)}>Agregar al Carrito</button>
+              <button onClick={() => dispatch(addToCart(product))}>Agregar al Carrito</button>
             )}
           </div>
         ))}
